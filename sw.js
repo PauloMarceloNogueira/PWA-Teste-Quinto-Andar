acc10b605f8cbfea7e6b55e779d20d06ceebe286
Paulo Marcelo <paulomarceloa.nogueira@gmail.com>
2019-02-05 15:43:36 -0200

    Updates

---
+++ sw.js
@@ -0,0 +1,29 @@
+const cacheVersion = "pwa-quintoandar-v1";
+const filesToCache = [
+  "/",
+  "/index.html",
+  "/dist/bundle.css",
+  "/dist/bundle.js"
+];
+
+self.addEventListener("install", function(e) {
+  console.log("[ServiceWorker] install");
+  e.waitUntil(
+    caches.open(cacheVersion).then(function(cache) {
+      console.log("[ServiceWorker] Caching app shell");
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true }).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
